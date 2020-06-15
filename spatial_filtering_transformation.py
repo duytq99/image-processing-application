ee18fa658728f1bf2e0f1b83774b8ea8d23d1bc9
duytranhcmut <duy.tran.bbl@gmail.com>
2020-06-16 00:12:34 +0700

    add output sample

--- spatial_filtering_transformation.py
+++ spatial_filtering_transformation.py
@@ -7,6 +7,7 @@ def avg_filter(img,size):
     kernel = np.ones((size,size),np.float32)
     kernel = kernel / (size**2)
     img_avg = cv2.filter2D(img,-1,kernel)
+    cv2.imwrite("avg_result.jpg",img_avg)
     return img_avg
 
 def weighted_avg_filter(img,b):
@@ -14,7 +15,7 @@ def weighted_avg_filter(img,b):
     kernel /=  (b+2)*(b+2)
     print(kernel)
     result = cv2.filter2D(img, -1, kernel)      
-    #box_filter_img = cv2.blur(scr,(size,size))
+    cv2.imwrite("wavg_result.jpg",result)
     return result
 
 def gaussian_filter(img,size,sigma):
@@ -25,20 +26,24 @@ def gaussian_filter(img,size,sigma):
     print(kernel)
     result = cv2.filter2D(img,-1,kernel)
     # result = cv2.GaussianBlur(img,(size,size),0)
+    cv2.imwrite("gauss_result.jpg",result)
     return result
 
 def median_filter(img,size):
     denoise_img = cv2.medianBlur(img,size)
+    cv2.imwrite("median_result.jpg",denoise_img)
     return denoise_img
 
 def laplacian_filter(img,size):
     result = cv2.Laplacian(img,cv2.CV_8U, None, size)
+    cv2.imwrite("laplace_result.jpg",result)
     return result
 
 def gab_filter(img,size=(15,15),lamda=10,theta=1,phi=0,sigma=5,gamma=1):
-    kernel = cv2.getGaborKernel((size, size), sigma, theta, lamda, gamma, phi, cv2.CV_32F)
+    kernel = cv2.getGaborKernel((15,15), sigma, theta, lamda, gamma, phi, cv2.CV_32F)
     kernel /= math.sqrt((kernel * kernel).sum())
     filtered = cv2.filter2D(img, -1, kernel)
+    cv2.imwrite("gabor_result.jpg",filtered)
     return filtered
 
 def sob_filter(img,ksize):
@@ -52,6 +57,7 @@ def sob_filter(img,ksize):
     abs_grad_y = cv2.convertScaleAbs(grad_y)
     grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
     print(ksize)
+    cv2.imwrite("sobel_result.jpg",grad)
     return grad
 
 def pre_filter(img):
@@ -60,20 +66,21 @@ def pre_filter(img):
     img_prewittx = cv2.filter2D(img, -1, kernelx)
     img_prewitty = cv2.filter2D(img, -1, kernely)
     result = img_prewittx + img_prewitty
+    cv2.imwrite("prewitt_result_gray.jpg",result)
     return result
 
 if __name__ == "__main__":
-    image_path = ""
-    img = cv2.imread(image_path,1)
+    image_path = "input/team.jpg"
+    img = cv2.imread(image_path,0)
 
     #img = avg_filter(img,size=15)
     #img = weighted_avg_filter(img,b=2)
     #img = gaussian_filter(img,5,1)
     #img = median_filter(img,5)
-    #img = laplacian_filter(img,size=3)
-    #img = np.uint8(gab_filter(img))
-    #img = sob_filter(img,3)
-    #img = pre_filter(img)
+    #mg = laplacian_filter(img,size=3)
+    #img = gab_filter(img)
+    #img = sob_filter(img,5)
+    img = pre_filter(img)
 
     cv2.imshow("img",img)
     cv2.waitKey(0)
